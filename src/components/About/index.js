eea0b69efbe7362eeb24815df54ad0a895cb6927
Neeko Tang <neeko_tvxq@hotmail.com>
2021-04-21 20:57:27 -0400

    Update App.js. About, Nav, Contact, Header, Footer

---
+++ src/components/About/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        </div>
+        <p>
+        After 5+ years of actively and passionately involved within a demonstrated history of working in the design industry. Strong arts and design professional graduated from University of Manitoba. Full Stack Certificate from University of Toronto.
+        </p>
+        <p>
+        As a creative professional, I have the ability to recognize solid talent. You can expect that I will understand your skills and will work to present them with opportunities that best match your talents and desires.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
