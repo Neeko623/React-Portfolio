14c2ff5b44c0de2b882c99e9f7d72c2df932c4a7
Neeko Tang <neeko_tvxq@hotmail.com>
2021-04-23 12:15:52 -0400

    Edited Portfolio information and update links

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Project from "../Project";
+
+function Portfolio() {
+
+  // Replace links with deployed projects and GitHub repos
+  const [projects] = useState([
+    {
+      name: 'Budget-Tracker',
+      description: 'PWA',
+      link: "https://budgettracker2021.herokuapp.com",
+      repo: "https://github.com/Neeko623/Budget-Tracker"
+    },
+    {
+      name: 'Milestone',
+      description: 'Full Stack',
+      link: "https://milestone-goals.herokuapp.com",
+      repo: "https://github.com/proj2gr4/Milestone-Project"
+    },
+    {
+      name: 'Run-Buddy',
+      description: 'HTML/CSS',
+      link: "https://neeko623.github.io/Run-Buddy-Website/",
+      repo: "https://github.com/Neeko623/Run-Buddy-Website"
+    },
+    {
+      name: 'Poke-Parte',
+      description: 'Project One',
+      link: "https://damn-ma-6.github.io/Poke-Parte/",
+      repo: "https://github.com/damn-ma-6/Poke-Parte"
+    },
+  ]);
+
+  return (
+    <div>
+      <div className="flex-row">
+        {projects.map((project, idx) => (
+          <Project
+            project={project}
+            key={"project" + idx}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
