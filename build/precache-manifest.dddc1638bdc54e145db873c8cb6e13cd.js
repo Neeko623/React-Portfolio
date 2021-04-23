6c65e13b5576d4fdfb49f477f8ff77924e17a13e
Neeko Tang <neeko_tvxq@hotmail.com>
2021-04-23 19:27:08 -0400

    RUN DEPLOY

---
+++ build/precache-manifest.dddc1638bdc54e145db873c8cb6e13cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aae150b1e807f628c23cf3c33da3d0d",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "5e65fc18cce4fa27ec57",
+    "url": "/React-Portfolio/static/css/main.d25199a4.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e65fc18cce4fa27ec57",
+    "url": "/React-Portfolio/static/js/main.60c45a87.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/React-Portfolio/static/media/Budget-Tracker.a19f2b67.jpg"
+  },
+  {
+    "revision": "35b292f540e86249df766d9046c3da69",
+    "url": "/React-Portfolio/static/media/Milestone.35b292f5.jpg"
+  },
+  {
+    "revision": "be5158841c6dab5f9ea8547c4f3f4f7e",
+    "url": "/React-Portfolio/static/media/Poke-Parte.be515884.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/React-Portfolio/static/media/Run-Buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "142fd461563d9e330b3fde5bd0c4f0ee",
+    "url": "/React-Portfolio/static/media/cover-image.142fd461.jpg"
+  }
+]);
